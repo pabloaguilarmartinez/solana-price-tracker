f3ae378c0f62dc93e0b5cb9c4ce003a9688b566b
pabloaguilarm <paguilar@heytrade.com>
2023-12-31 19:20:46 +0100

    Modifying gradient colors

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import solanaLogo from './assets/solana-logotype.svg';
 function App() {
   return (
     <>
-      <header className="bg-gradient-to-r from-teal-500 to-purple-500 p-4">
+      <header className="bg-gradient-to-r from-teal-500 via-cyan-500 via-47% to-purple-500 to-52% p-4">
         <h1 className="font-mono">SOLANA PRICE TRACKER</h1>
       </header>
       <main className="bg-black text-white min-h-screen flex flex-col justify-center items-center p-4">
         <div className="w-full max-w-md mx-auto">
           <div
-            className="bg-gradient-to-r from-purple-400 via-purple-500 via-30% to-gray-950 text-white border-transparent border-r-black rounded">
+            className="bg-gradient-to-r from-cyan-400 via-blue-900 via-70% to-gray-950 text-white border-transparent border-r-black rounded">
             <div className="flex items-center justify-between p-8">
               <img src={solanaLogo} alt="solana-logo" className="w-40 ml-24" />
             </div>
