8f0fa997719740732492d97d15b63138c838c03e
pabloaguilarm <paguilar@heytrade.com>
2023-12-31 18:52:50 +0100

    Adding html and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
-
+import solanaLogo from './assets/solana-logotype.svg';
 
 function App() {
   return (
     <>
+      <header className="bg-gradient-to-r from-teal-500 to-purple-500 p-4">
+        <h1 className="font-mono">SOLANA PRICE TRACKER</h1>
+      </header>
+      <main className="bg-black text-white min-h-screen flex flex-col justify-center items-center p-4">
+        <div className="w-full max-w-md mx-auto">
+          <div
+            className="bg-gradient-to-r from-purple-400 via-purple-500 via-30% to-gray-950 text-white border-transparent border-r-black rounded">
+            <div className="flex items-center justify-between p-8">
+              <img src={solanaLogo} alt="solana-logo" className="w-40 ml-24" />
+            </div>
+            <div className="flex items-center justify-between py-6 px-6">
+              <div className="flex flex-col gap-1">
+                <h3 className="text-xl font-semibold">Current Price</h3>
+                <p className="text-sm text-gray-200">Updated every 10 seconds</p>
+              </div>
+              <div>
+                <h4 className="text-3xl font-bold">$3,456.78</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
