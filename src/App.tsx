b056452c3d2c8ac4021c1f04475c7e171729e04b
pabloaguilarm <paguilar@heytrade.com>
2023-12-31 20:06:03 +0100

    Fetching price

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
 import solanaLogo from './assets/solana-logotype.svg';
+import {useEffect, useState} from "react";
+
+const API_URL: string = 'https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd&precision=2';
+
+type CoinGeckoResponse = {
+  solana: {
+    usd: number
+  }
+};
+
+const getPrice = async (): Promise<number> => {
+  const response = await fetch(API_URL)
+  const json: CoinGeckoResponse = await response.json();
+  return json.solana.usd;
+};
 
 function App() {
+  const [price, setPrice] = useState<number>(0.00);
+
+  const updatePrice = (): void => {
+    getPrice().then(currentPrice => {
+      setPrice(currentPrice);
+    });
+  };
+  useEffect(() => {
+    updatePrice();
+    return () => clearInterval(setInterval(updatePrice, 10000));
+  }, []);
   return (
     <>
       <header className="bg-gradient-to-r from-teal-500 via-cyan-500 via-47% to-purple-500 to-52% p-4">
@@ -11,7 +37,7 @@ function App() {
           <div
             className="bg-gradient-to-r from-cyan-400 via-blue-900 via-70% to-gray-950 text-white border-transparent border-r-black rounded">
             <div className="flex items-center justify-between p-8">
-              <img src={solanaLogo} alt="solana-logo" className="w-40 ml-24" />
+              <img src={solanaLogo} alt="solana-logo" className="w-40 ml-24"/>
             </div>
             <div className="flex items-center justify-between py-6 px-6">
               <div className="flex flex-col gap-1">
@@ -19,7 +45,7 @@ function App() {
                 <p className="text-sm text-gray-200">Updated every 10 seconds</p>
               </div>
               <div>
-                <h4 className="text-3xl font-bold">$3,456.78</h4>
+                <h4 className={`text-3xl font-bold`}>${price}</h4>
               </div>
             </div>
           </div>
